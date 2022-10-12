b1b46defc31c9e8747ae424b252e69f5867ca955
Frederico Lima Alevi <fredealevi@hotmail.com>
2022-10-12 19:44:52 -0300

    Finalizado Reserva de Emergencia V1.0

--- Controllers/ReservaEmergenciaController.cs
+++ Controllers/ReservaEmergenciaController.cs
@@ -2,11 +2,106 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Controle_Financeiro.ContextDb;
+using Controle_Financeiro.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Controle_Financeiro.Controllers
 {
-    public class ReservaEmergenciaController
+    public class ReservaEmergenciaController : Controller
     {
-        
+        private readonly BancoDbContext _tabela;
+        public ReservaEmergenciaController(BancoDbContext banco)
+        {
+            _tabela = banco;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var depositos = _tabela.ReservaEmergencia.ToList();
+
+            return View(depositos);
+        }
+
+        [HttpGet]
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(ReservaEmergencia reserva)
+        {
+            if(ModelState.IsValid)
+            {
+                _tabela.ReservaEmergencia.Add(reserva);
+                _tabela.SaveChanges();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(reserva);
+        }
+
+        [HttpGet]
+        public IActionResult Editar(int id)
+        {
+            var reserva = _tabela.ReservaEmergencia.Find(id);
+
+            if(reserva == null) 
+                return NotFound();
+
+            return View(reserva);
+        }
+
+        [HttpPost]
+        public IActionResult Editar(ReservaEmergencia reservaAtualizada)
+        {
+            var reservaEmergencia = _tabela.ReservaEmergencia.Find(reservaAtualizada.Id);
+
+            reservaEmergencia.Valor = reservaAtualizada.Valor;
+            reservaEmergencia.Tipo = reservaAtualizada.Tipo;
+
+            _tabela.ReservaEmergencia.Update(reservaEmergencia);
+            _tabela.SaveChanges();
+
+            return RedirectToAction(nameof(Index)); 
+        }
+
+        [HttpGet]
+        public IActionResult Detalhes(int id)
+        {
+            var reserva = _tabela.ReservaEmergencia.Find(id);
+
+            if (reserva == null)
+                return RedirectToAction(nameof(Index));
+
+            return View(reserva);
+        }
+
+        [HttpGet]
+        public IActionResult Deletar(int id)
+        {
+            var reserva = _tabela.ReservaEmergencia.Find(id);
+
+            if( reserva == null)
+                return RedirectToAction(nameof(Index));
+            
+            return View(reserva);
+        }
+
+        [HttpPost]
+        public IActionResult Deletar(ReservaEmergencia reserva)
+        {
+            var reservaDeletar = _tabela.ReservaEmergencia.Find(reserva.Id);
+
+            _tabela.ReservaEmergencia.Remove(reservaDeletar);
+            _tabela.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }   
+
     }
+
 }
--- Models/ReservaEmergencia.cs
+++ Models/ReservaEmergencia.cs
@@ -9,8 +9,6 @@ namespace Controle_Financeiro.Models
     {
         public int Id { get; set; }
 
-        DateTime dataEntrada {get; set;}
-
         public decimal Valor {get; set;}
 
         public string Tipo {get; set;}
